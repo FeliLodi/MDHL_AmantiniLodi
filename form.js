843b590d02c71a60eed2d8f265a536aa9dffed4a
makubestyoonginista <90396372+makubestyoonginista@users.noreply.github.com>
2023-11-29 14:18:49 -0300

    conexion de base de datos

---
+++ form.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var contacto = document.getElementById('formmdhl');
+  
+    contacto.addEventListener('submit', (event) => {
+      event.preventDefault();
+  
+      var firstname = document.getElementById("firstname").value;
+      var lastname = document.getElementById("lastname").value;
+      var streetaddress = document.getElementById("streetaddress").value;
+      var city = document.getElementById("city").value;
+      var zipcode = document.getElementById("zipcode").value;
+      var birthdate = document.getElementById("birthdate").value;
+      
+      var datosEnviar = {
+        firstname: firstname,
+        lastname: lastname,
+        streetaddress: streetaddress,
+        city: city,
+        zipcode: zipcode,
+        birthdate: birthdate,
+      };
+  
+      fetch('./form.php', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(datosEnviar)
+      })
+      .then(response => response.text())
+      .then(data => {
+        console.log(data);
+        contacto.reset(); 
+      })
+      .catch(error => {
+        console.error(error);
+      });
+    });
+  });
---
+++ form.php
@@ -0,0 +1,48 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+
+$method = isset($_SERVER['REQUEST_METHOD']) ? $_SERVER['REQUEST_METHOD'] : '';
+
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "inscripcion";
+
+$connection = mysqli_connect($servername, $username, $password, $dbname);
+
+if ($connection->connect_error) {
+    die("Error de conexión: " . $connection->connect_error);
+}
+
+// Verificar si los datos están presentes antes de intentar acceder a ellos
+$data = json_decode(file_get_contents('php://input'), true);
+
+if (!empty($data['firstname']) && !empty($data['lastname']) && !empty($data['streetaddress']) && !empty($data['city']) && !empty($data['zipcode']) && !empty($data['birthdate']) && !empty($data['id'])) {
+    
+    $firstname = $data['firstname'];
+    $lastname = $data['lastname'];
+    $streetaddress = $data['streetaddress'];
+    $city = $data['city'];
+    $zipcode = $data['zipcode'];
+    $birthdate = $data['birthdate'];
+    $id = $data['id'];
+
+    if ($method === 'POST') {
+    
+        $insertQuery = "INSERT INTO formmdhl(firstname, lastname, streetaddress, city, zipcode, birthdate, id) VALUES ('$firstname', '$lastname', '$streetaddress', '$zipcode', '$city', '$birthdate', '$id')";
+        
+        if (mysqli_query($connection, $insertQuery)) {
+            $response = array('status' => 'success', 'message' => 'Datos insertados correctamente');
+            echo json_encode($response);
+        } else {
+            $response = array('status' => 'error', 'message' => 'Error al insertar datos: ' . mysqli_error($connection));
+            echo json_encode($response);
+        }
+    }
+}
+    mysqli_close($connection);
+?>
